b98a8e49aa02ee816975fd9cfcdbef92ff958da3
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-06-29 23:10:20 -0300

    feat: Add success page

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,3 +1,56 @@
+import { MapPin, Timer, CurrencyDollar } from '@phosphor-icons/react'
+
+import MotoboyDelivery from '../assets/motoboy-delivery.svg'
+
 export function Success() {
-  return <h1>Success</h1>
+  return (
+    <main>
+      <div>
+        <div>
+          <h1>Uhu! Pedido confirmado</h1>
+          <p>Agora é só aguardar que logo o café chegará até você</p>
+        </div>
+
+        <ul>
+          <li>
+            <span>
+              <MapPin weight="fill" />
+            </span>
+            <div>
+              <p>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+              </p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </li>
+
+          <li>
+            <span>
+              <Timer weight="fill" />
+            </span>
+            <div>
+              <p>Previsão de entrega</p>
+              <p>
+                <strong>20 min - 30 min</strong>
+              </p>
+            </div>
+          </li>
+
+          <li>
+            <span>
+              <CurrencyDollar />
+            </span>
+            <div>
+              <p>Pagamento na entrega</p>
+              <p>
+                <strong>Cartão de Crédito</strong>
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <img src={MotoboyDelivery} alt="" />
+    </main>
+  )
 }
