bdaddb4308d7dfc4739fe1af81297a73f18f51ae
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-06-29 23:14:51 -0300

    feat(Header): add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import { HeaderContainer } from './styles'
+
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+
+import CoffeeDeliveryLogo from '../assets/coffee-delivery-logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={CoffeeDeliveryLogo} alt="" />
+
+      <div>
+        <button title="Local">
+          <MapPin weight="fill" /> Porto Alegre, RS
+        </button>
+
+        <button title="Carrinho">
+          <span>3</span>
+          <ShoppingCart weight="fill" />
+        </button>
+      </div>
+    </HeaderContainer>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import {
-  MapPin,
   ShoppingCart,
   Package,
   Timer,
@@ -8,110 +7,92 @@ import {
   Minus,
 } from '@phosphor-icons/react'
 
-import CoffeeDeliveryLogo from '../assets/coffee-delivery-logo.svg'
 import CoffeeBanner from '../assets/coffee-banner.svg'
 import Expresso from '../assets/expresso.svg'
 
 export function Home() {
   return (
-    <div>
-      <header>
-        <img src={CoffeeDeliveryLogo} alt="" />
-
+    <main>
+      <div>
         <div>
-          <button title="Local">
-            <MapPin weight="fill" /> Porto Alegre, RS
-          </button>
-
-          <button title="Carrinho">
-            <span>3</span>
-            <ShoppingCart weight="fill" />
-          </button>
-        </div>
-      </header>
-
-      <main>
-        <div>
-          <div>
-            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-            <p>
-              Com o Coffee Delivery você recebe seu café onde estiver, a
-              qualquer hora
-            </p>
-
-            <ul>
-              <li>
-                <span>
-                  <ShoppingCart weight="fill" />
-                </span>
-                Compra simples e segura
-              </li>
-
-              <li>
-                <span>
-                  <Package weight="fill" />
-                </span>
-                Embalagem mantém o café intacto
-              </li>
-
-              <li>
-                <span>
-                  <Timer weight="fill" />
-                </span>
-                Entrega rápida e rastreada
-              </li>
-
-              <li>
-                <span>
-                  <Coffee weight="fill" />
-                </span>
-                O café chega fresquinho até você
-              </li>
-            </ul>
-          </div>
-
-          <img src={CoffeeBanner} alt="" />
-        </div>
-
-        <div>
-          <h2>Nossos cafés</h2>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
 
           <ul>
             <li>
-              <img src={Expresso} alt="" />
+              <span>
+                <ShoppingCart weight="fill" />
+              </span>
+              Compra simples e segura
+            </li>
 
-              <span>TRADICIONAL</span>
+            <li>
+              <span>
+                <Package weight="fill" />
+              </span>
+              Embalagem mantém o café intacto
+            </li>
 
-              <div>
-                <strong>Expresso Tradicional</strong>
-                <p>O tradicional café feito com água quente e grãos moídos</p>
-              </div>
+            <li>
+              <span>
+                <Timer weight="fill" />
+              </span>
+              Entrega rápida e rastreada
+            </li>
 
-              <div>
-                <span>
-                  R$ <strong>9,90</strong>
-                </span>
-
-                <div>
-                  <div>
-                    <button title="Aumentar quantidade desejada">
-                      <Minus />
-                    </button>
-                    <span>1</span>
-                    <button title="Diminuir quantidade desejada">
-                      <Plus />
-                    </button>
-                  </div>
-
-                  <button title="Adicionar ao carrinho">
-                    <ShoppingCart />
-                  </button>
-                </div>
-              </div>
+            <li>
+              <span>
+                <Coffee weight="fill" />
+              </span>
+              O café chega fresquinho até você
             </li>
           </ul>
         </div>
-      </main>
-    </div>
+
+        <img src={CoffeeBanner} alt="" />
+      </div>
+
+      <div>
+        <h2>Nossos cafés</h2>
+
+        <ul>
+          <li>
+            <img src={Expresso} alt="" />
+
+            <span>TRADICIONAL</span>
+
+            <div>
+              <strong>Expresso Tradicional</strong>
+              <p>O tradicional café feito com água quente e grãos moídos</p>
+            </div>
+
+            <div>
+              <span>
+                R$ <strong>9,90</strong>
+              </span>
+
+              <div>
+                <div>
+                  <button title="Aumentar quantidade desejada">
+                    <Minus />
+                  </button>
+                  <span>1</span>
+                  <button title="Diminuir quantidade desejada">
+                    <Plus />
+                  </button>
+                </div>
+
+                <button title="Adicionar ao carrinho">
+                  <ShoppingCart />
+                </button>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
   )
 }
